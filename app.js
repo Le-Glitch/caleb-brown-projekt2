1843d672b7d84cbc081906e15af06d5febb5c630
Glitch <caleb.samuel.brown@gmail.com>
2022-02-08 14:18:07 +0100

    fixade på något sätt

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-let cookies = 0
-let cps = 0
-let
+let cookieAmount = 0,
+  cookieClick = 1;
+let cpsAmount = 0,
+  cps = document.querySelector(".cps");
+
+let cookieButton = document.querySelector(".cookieButton");
+let cookieCounter = document.querySelector(".cookieCounter");
+
+cookieButton.addEventListener("click", cookie);
+
+function cookie() {
+  cookieAmount += cookieClick;
+
+  cookieCounter.innerHTML = cookieAmount;
+}
