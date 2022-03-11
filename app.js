6a5483f22fb5c7c4f19151feb2aec2adc0cb9647
Glitch <caleb.samuel.brown@gmail.com>
2022-03-11 12:55:12 +0100

    final product

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let title = document.querySelector("title");
 let cookieImg = document.getElementById("cookieImg")[0];
 
-let cookieAmount = 15,
+let cookieAmount = 0,
   cookieClick = 1,
   cpsAmount = 0;
 
@@ -23,30 +23,6 @@ let clickerButton = document.querySelector(".clickerButton"),
 
 clickerButton.addEventListener("click", ClickerBuy);
 
-let grandmaAmount = 0,
-  grandmaCost = 100,
-  grandmaCPS = 1;
-
-let grandmaButton = document.querySelector(".grandmaButton"),
-  grandmaCounter = document.querySelector(".grandmaAmount"),
-  grandmaPrice = document.querySelector(".grandmaPrice");
-
-grandmaButton.addEventListener("click", GrandmaBuy);
-
-function CookieClick() {
-  cookieAmount += cookieClick;
-
-  cookieCounter.innerHTML = Math.floor(cookieAmount);
-}
-
-setInterval(function () {
-  cookieAmount += cpsAmount;
-
-  cookieCounter.innerHTML = Math.floor(cookieAmount);
-
-  title.innerHTML = Math.floor(cookieAmount) + " cookies - Cookie Clicker";
-}, 1000);
-
 function ClickerBuy() {
   if (cookieAmount >= clickerCost) {
     clickerAmount++;
@@ -64,6 +40,18 @@ function ClickerBuy() {
   }
 }
 
+
+
+let grandmaAmount = 0,
+  grandmaCost = 100,
+  grandmaCPS = 1;
+
+let grandmaButton = document.querySelector(".grandmaButton"),
+  grandmaCounter = document.querySelector(".grandmaAmount"),
+  grandmaPrice = document.querySelector(".grandmaPrice");
+
+grandmaButton.addEventListener("click", GrandmaBuy);
+
 function GrandmaBuy() {
   if (cookieAmount >= grandmaCost) {
     grandmaAmount++;
@@ -80,3 +68,51 @@ function GrandmaBuy() {
     grandmaPrice.innerHTML = grandmaCost;
   }
 }
+
+
+
+let factoryAmount = 0,
+  factoryCost = 1100,
+  factoryCPS = 8;
+
+let factoryButton = document.querySelector(".factoryButton"),
+  factoryCounter = document.querySelector(".factoryAmount"),
+  factoryPrice = document.querySelector(".factoryPrice");
+
+factoryButton.addEventListener("click", FactoryBuy);
+
+function FactoryBuy() {
+  if (cookieAmount >= factoryCost) {
+    factoryAmount++;
+    cpsAmount += factoryCPS;
+    cookieAmount -= factoryCost;
+
+    factoryCost = Math.round(
+      (factoryCost * upgradeMultiplier) ^ (factoryAmount + 1)
+    );
+
+    cps.innerHTML = parseFloat(cpsAmount.toFixed(1));
+    cookieCounter.innerHTML = Math.floor(cookieAmount);
+    factoryCounter.innerHTML = factoryAmount;
+    factoryPrice.innerHTML = factoryCost;
+  }
+}
+
+
+
+function CookieClick() {
+  cookieAmount += cookieClick;
+
+  cookieCounter.innerHTML = Math.floor(cookieAmount);
+}
+
+setInterval(function () {
+  cookieAmount += cpsAmount;
+
+  cookieCounter.innerHTML = Math.floor(cookieAmount);
+
+  title.innerHTML = Math.floor(cookieAmount) + " cookies - Cookie Clicker";
+}, 1000);
+
+
+
